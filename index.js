ea8647f441914949ecf9416b054451f7a8b37ff0
lsc1634 <metalfunk9@gmail.com>
2022-04-07 09:18:37 +0900

    비밀정보 보호

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require("./models/User");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kastard:!atmxpffk159@kastard.hwfjh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+const { json } = require('express/lib/response');
+mongoose.connect(config.mongoURI, {
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateindex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
@@ -12,6 +22,22 @@ mongoose.connect('mongodb+srv://kastard:!atmxpffk159@kastard.hwfjh.mongodb.net/m
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+
+app.post('/register', (req, res) => {
+  //회원가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+      if (err) return res.json({ success: false, err })
+      return res.status(200).json({
+        success: true
+      })
+    })
+
 })
 
 app.listen(port, () => {
